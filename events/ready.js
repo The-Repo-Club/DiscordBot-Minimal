d04d147fa52a703c4e9247b0d762223688551498
The-Repo-Club <wayne6324@gmail.com>
2022-02-23 19:52:28 +0000

    Update

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,29 @@
-const chalk = require('chalk');
-const ms = require('ms');
+const chalk = require("chalk");
+const ms = require("ms");
 
-module.exports = async(client) => {
-    setTimeout(async function() {
-        console.log(chalk.white(`[${chalk.blueBright("LINKNSYNC-CLIENT")}]${chalk.white(" - ")}${chalk.blue("Connecting...")}`));
-    }, ms("0.2s"));
-    setTimeout(async function() {
-        console.log(chalk.white(`[${chalk.blueBright("LINKNSYNC-CLIENT")}]${chalk.white(" - ")}${chalk.blue(`Connected to ${client.user.tag}`)}`))
-        console.log(" ")
-    }, ms("1s"));
-    console.log(" ")
+module.exports = async (client) => {
+	setTimeout(async function () {
+		console.log(
+			chalk.white(
+				`[${chalk.blueBright("LINKNSYNC-CLIENT")}]${chalk.white(
+					" - "
+				)}${chalk.blue("Connecting...")}`
+			)
+		);
+	}, ms("0.2s"));
+	setTimeout(async function () {
+		console.log(
+			chalk.white(
+				`[${chalk.blueBright("LINKNSYNC-CLIENT")}]${chalk.white(
+					" - "
+				)}${chalk.blue(`Connected to ${client.user.tag}`)}`
+			)
+		);
+		console.log(" ");
+	}, ms("1s"));
+	console.log(" ");
 
-    await client.user.setPresence({ activities: [{ name: "in development | v1.0.0" }] });
-}
+	await client.user.setPresence({
+		activities: [{ name: "for suggestions", type: "WATCHING" }],
+	});
+};
